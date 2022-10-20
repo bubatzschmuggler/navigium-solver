51ccd4fc0f00227556e1ce1def55b5e89af8bdbf
Gabriel <92476790+github-gabriel@users.noreply.github.com>
2022-10-20 12:19:22 +0200

    Update navigium.py

--- navigium.py
+++ navigium.py
@@ -3,6 +3,7 @@ import time
 import random
 import urllib.parse
 import requests
+import selenium.common.exceptions
 
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -39,7 +40,7 @@ class Main:
         self.driver = webdriver.Chrome()
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.chrome_options)
 
-        self.page_load_delay = 6
+        self.page_load_delay = 4
         self.action_delay = 1
 
         self.unnecessary_checkboxes = {6, 7, 8, 9, 10, 11}
@@ -64,35 +65,45 @@ class Main:
             i.click()
 
     def exit(self):
-        time.sleep(1)
+        time.sleep(10)
+        print("Wird beendet...")
         self.driver.quit()
         sys.exit(0)
 
     def end_quiz(self):
-        self.find_element_selenium(By.CSS_SELECTOR, ".btn.btn-danger-outline.btn-sm.pull-right", 1).click()
-        self.find_element_selenium(By.CSS_SELECTOR, ".btn.btn-primary.btn-sm.ng-star-inserted", 2.5).click()
+        print("Beende Vokabel lernen...")
+        try:
+            self.find_element_selenium(By.CSS_SELECTOR, ".btn.btn-danger-outline.btn-sm.pull-right",
+                                       self.action_delay).click()
+        except selenium.common.exceptions.NoSuchElementException:
+            pass
+        self.find_element_selenium(By.CSS_SELECTOR, ".btn.btn-primary.btn-sm.ng-star-inserted",
+                                   self.action_delay).click()
         self.driver.save_screenshot("result.png")
         self.find_element_selenium(By.CSS_SELECTOR, ".btn.btn-primary-outline.btn-sm.hidden-print.ng-star-inserted",
-                                   2.5).click()
+                                   self.action_delay).click()
         self.exit()
 
     def vokabeln(self):
         time.sleep(6)
-        while True:
-            buttons = self.driver.find_elements(By.CSS_SELECTOR, ".btn.btn-default.abstandlinks")
-            if len(buttons) > 0:
-                try:
-                    vokabel = self.find_element_selenium(By.XPATH,
-                                                         "/html/body/app-root/app-schriftlich/div/div/div[2]/div[1]/div/div/div/h4[1]",
-                                                         1).get_attribute("innerText").strip()
-                    loesungen = search_vokabel(vokabel)
-
+        try:
+            while "Was bedeutet diese Vokabel?" in self.find_element_selenium(By.TAG_NAME, "h5", 0).get_attribute(
+                    "innerText"):
+                buttons = self.driver.find_elements(By.CSS_SELECTOR, ".btn.btn-default.abstandlinks")
+                vokabel = self.find_element_selenium(By.XPATH,
+                                                     "/html/body/app-root/app-schriftlich/div/div/div[2]/div[1]/div/div/div/h4[1]",
+                                                     1).get_attribute("innerText").strip()
+                loesungen = search_vokabel(vokabel)
+                if "Was bedeutet diese Vokabel?" in self.find_element_selenium(By.TAG_NAME, "h5", 0).get_attribute(
+                        "innerText"):
                     click_correct_button(buttons, loesungen)
-
-                    time.sleep(2.5)
-                    self.find_element_selenium(By.CSS_SELECTOR, ".btn.btn-primary.btn-sm.ng-star-inserted", 2).click()
-                except:
+                    time.sleep(0.5)
+                    self.find_element_selenium(By.CSS_SELECTOR, ".btn.btn-primary.btn-sm.ng-star-inserted",
+                                               self.action_delay).click()
+                else:
                     self.end_quiz()
+        except selenium.common.exceptions.NoSuchElementException:
+            self.end_quiz()
 
     def main(self):
         self.load_new_page("https://www.navigium.de/schule/login/mainmenu.html")
